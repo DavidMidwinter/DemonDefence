3017c4c20d3b398810a07aa126c64363efea34d4
David <david.midwinter@btopenworld.com>
2024-05-01 11:26:19 +0100

    refactor: make grid square

--- DemonDefence/Assets/GridManager.cs
+++ DemonDefence/Assets/GridManager.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
-    [SerializeField] private int _width, _height;
+    [SerializeField] private int _gridSize;
     [SerializeField] private Tile _tilePrefab;
     [SerializeField] private Dictionary<Vector2, Tile> _tiles;
-
     private void Start()
     {
         GenerateGrid();
+
     }
 
     void GenerateGrid()
     {
         _tiles = new Dictionary<Vector2, Tile>();
-        for (int x = 0; x < _width; x++)
+        for (int x = 0; x < _gridSize; x++)
         {
-            for (int z = 0; z < _height; z++)
+            for (int z = 0; z < _gridSize; z++)
             {
                 Vector3 location = new Vector3(10 * x, 0, 10 * z);
                 var spawnedTile = Instantiate(_tilePrefab, location, Quaternion.identity);
