67ea4c64aa84ea295be658ccc997574e46d2b4da
David <david.midwinter@btopenworld.com>
2024-05-02 18:07:16 +0100

    feat: add movement limit

--- DemonDefence/Assets/Scripts/Units/BaseUnit.cs
+++ DemonDefence/Assets/Scripts/Units/BaseUnit.cs
@@ -6,4 +6,10 @@ public class BaseUnit : MonoBehaviour
 {
     public Tile OccupiedTile;
     public Faction faction;
+    public int maxMovement;
+    public bool isInRange(Vector3 location)
+    {
+        return (location - transform.position).magnitude <= maxMovement * 10;
+
+    }
 }
