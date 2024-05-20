55d05c72c03e544c972b2cc4b9054e69c2dfd121
David <david.midwinter@btopenworld.com>
2024-05-20 09:59:58 +0100

    feat: create calculateThreshold method

--- DemonDefence/Assets/Scripts/Utils.cs
+++ DemonDefence/Assets/Scripts/Utils.cs
@@ -10,4 +10,23 @@ public static class Utils
         TacticalUI.Instance.setCardText($" {result} ");
         return result;
     }
+
+    public static int calculateThreshold(int strength, int toughness)
+    {
+        /// Calculate the roll threshold.
+        /// First, set threshold to 5. Rolls must exceed this.
+        /// Subtract 'strength' from 'toughness' and add the difference to threshold.
+        /// If threshold exceeds 9, set to 9; if it subceeds 1, set to 1.
+        /// Return threshold.
+
+        int threshold = 5;
+
+        int diff = toughness - strength;
+
+        threshold += diff;
+        if (threshold > 9) threshold = 9;
+        else if (threshold < 1) threshold = 1;
+
+        return threshold;
+    }
 }
