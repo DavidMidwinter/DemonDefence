2215f6f445b02f7361a6e9b0832297165fcca748
David <david.midwinter@btopenworld.com>
2024-06-20 16:25:50 +0100

    refactor: set menu name for scriptable detachment

--- DemonDefence/Assets/Scripts/Units/ScriptableDetachment.cs
+++ DemonDefence/Assets/Scripts/Units/ScriptableDetachment.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Unit", menuName = "Scriptable Unit")]
+[CreateAssetMenu(fileName = "New Unit", menuName = "Scriptable Detachment")]
 public class ScriptableDetachment : ScriptableObject
 {
     public Faction Faction;
