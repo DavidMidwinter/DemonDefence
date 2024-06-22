1be2278b9413553d95cface1be6e1fcaabc64cf8
David <david.midwinter@btopenworld.com>
2024-06-22 22:39:06 +0100

    feat: add a 'Greater Demon' as just a larger demon with more health. No abilities

---
+++ DemonDefence/Assets/Scripts/Units/EnemyUnits/GreaterDemon.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreaterDemon : BaseEnemyUnit
+{
+    /// Functionality unique to the Greater Demon unit
+}
