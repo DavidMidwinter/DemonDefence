e8692768a29765f4822cfc60efcfd23b74416fb4
David <david.midwinter@btopenworld.com>
2024-07-05 23:37:29 +0100

    feat: randomly rotate trees and make them unwalkable

--- DemonDefence/Assets/Scripts/Tiles/TreeTile.cs
+++ DemonDefence/Assets/Scripts/Tiles/TreeTile.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class TreeTile : GrassTile
 {
+    [SerializeField] private GameObject tree;
 
+    new public void Awake()
+    {
+        Quaternion randomRotation = Random.rotation;
+        tree.transform.rotation = new Quaternion(
+            0, randomRotation.y, 0, randomRotation.w);
+        base.Awake();
+    }
 }
