2083e15c69697f336b04d811cd9422f82a956976
David <david.midwinter@btopenworld.com>
2024-07-06 14:31:34 +0100

    feat: randomise scale of trees and bushes; randomise rotation of bushes

--- DemonDefence/Assets/Scripts/Tiles/BushTile.cs
+++ DemonDefence/Assets/Scripts/Tiles/BushTile.cs
@@ -4,5 +4,17 @@ using UnityEngine;
 
 public class BushTile : GrassTile
 {
-    
+
+    [SerializeField] private GameObject bush;
+
+    new public void Awake()
+    {
+        Quaternion randomRotation = Random.rotation;
+        bush.transform.rotation = new Quaternion(
+            0, randomRotation.y, 0, randomRotation.w);
+
+        float scale = Random.Range(0.8f, 1.1f);
+        bush.transform.localScale *= scale;
+        base.Awake();
+    }
 }
--- DemonDefence/Assets/Scripts/Tiles/TreeTile.cs
+++ DemonDefence/Assets/Scripts/Tiles/TreeTile.cs
@@ -11,6 +11,9 @@ public class TreeTile : GrassTile
         Quaternion randomRotation = Random.rotation;
         tree.transform.rotation = new Quaternion(
             0, randomRotation.y, 0, randomRotation.w);
+
+        float scale = Random.Range(1, 1.5f);
+        tree.transform.localScale *= scale;
         base.Awake();
     }
 }
