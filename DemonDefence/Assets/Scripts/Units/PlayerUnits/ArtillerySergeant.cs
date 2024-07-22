4c8a2716b702d22380db17d208f920f56a553668
David <david.midwinter@btopenworld.com>
2024-07-22 15:50:07 +0100

    feat: add artillery sergeant

---
+++ DemonDefence/Assets/Scripts/Units/PlayerUnits/ArtillerySergeant.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtillerySergeant : BasePlayerLeader
+{
+
+    
+    public override void onSelect()
+    {
+        Debug.Log($"Artillery Sergeant: {givenOrders} given out of {maxOrders} orders");
+        if(givenOrders < maxOrders)
+        {
+            getAffected(maxMovement);
+            base.onSelect();
+            TacticalUI.Instance.addAction("Indirect Fire", indirectFire);
+            TacticalUI.Instance.addAction("Strike", applyStrengthBonus);
+            TacticalUI.Instance.addAction("Point-Blank\nFire", allowPointBlankFire);
+        }
+        else base.onSelect();
+    }
+
+    public void indirectFire()
+    {
+        Debug.LogWarning("Not implemented");
+    }
+    public void allowPointBlankFire()
+    {
+        StartCoroutine(giveOrder(mr: -3));
+    }
+}
