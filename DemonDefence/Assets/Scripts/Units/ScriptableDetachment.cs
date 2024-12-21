b9cb12080575dd1bca918bd37912e09cb07fc3f0
David <david.midwinter@btopenworld.com>
2024-12-21 12:41:28 +0000

    feat: use scriptable unit

--- DemonDefence/Assets/Scripts/Units/ScriptableDetachment.cs
+++ DemonDefence/Assets/Scripts/Units/ScriptableDetachment.cs
@@ -2,15 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Unit", menuName = "Scriptable Detachment")]
+[CreateAssetMenu(fileName = "New Detachment", menuName = "Scriptable Detachment")]
 public class ScriptableDetachment : ScriptableObject
 {
     public string unitName;
     public Faction Faction;
-    public BaseUnit troopUnit;
+    public ScriptableUnit troopUnit;
     public int numberOfTroops;
-    public Texture2D troopImage;
-    public BaseUnit leaderUnit;
-    public Texture2D leaderImage;
-    public string[] leaderAbilities;
+    public ScriptableUnit leaderUnit;
 }
