4ea6f5f7f76185385d8d26e8b1715964e0f6240f
David <david.midwinter@btopenworld.com>
2024-12-31 13:48:17 +0000

    feat: add organ gun unit and detachment

---
+++ DemonDefence/Assets/Scripts/Units/PlayerUnits/OrganGun.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrganGun : BasePlayerUnit
+{
+    public override void applyModifiers(int move = 0, int str = 0, int tough = 0, int dmg = 0, int attack = 0, int minrange = 0, int maxrange = 0, bool indirectFire = false)
+    {
+        base.applyModifiers(move, str, tough, dmg, attack, minrange, maxrange, indirectFire);
+    }
+
+    public override void resetStats()
+    {
+        Debug.Log($"Reset Field Gun: {this}");
+        base.resetStats();
+    }
+}
