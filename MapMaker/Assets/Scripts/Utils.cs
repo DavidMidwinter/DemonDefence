ba94de64456b60c7556a77f9fb4fbf8ec276860b
David <david.midwinter@btopenworld.com>
2025-01-01 18:30:37 +0000

    feat: add necessary utils

--- MapMaker/Assets/Scripts/Utils.cs
+++ MapMaker/Assets/Scripts/Utils.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
 
 public static class Utils
 {
+    public static string saveDirectory = Path.Combine(Application.dataPath, "Maps");
     public static float calculateDistance(Vector2 origin, Vector2 target)
     {
         /// Calculate the distance between two 2D points
@@ -14,5 +17,20 @@ public static class Utils
         ///     float: The distance
         ///     
         return (origin - target).magnitude;
+    }
+
+    public static void loadEditor()
+    {
+        SceneManager.LoadScene("MapMaker");
+
+    }
+    public static void returnToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public static void exit()
+    {
+        Application.Quit();
     }
 }
